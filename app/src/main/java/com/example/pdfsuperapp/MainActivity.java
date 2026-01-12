a69839ed1c1ab224ba874fbc8f3fb1f354705183
sheikhsadi12 <sadishekh671@gmail.com>
2026-01-12 17:53:02 +0600

    Added AndroidManifest and MainActivity

---
+++ app/src/main/java/com/example/pdfsuperapp/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.pdfsuperapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
